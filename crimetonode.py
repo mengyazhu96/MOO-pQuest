c4578d230a7c58d3e83a43dde6fcac00418d47fc
Jessica <mengyazhu96@gmail.com>
2015-04-21 15:46:29 +0000

    to pull to local

--- crimetonode.py
+++ crimetonode.py
@@ -1,18 +1,19 @@
 # nodetoway.py
 #
-# takes in an OSM file, inserts a crime parameter into NODES and outputs an [XML OR OSM] file
+# takes in an OSM file (command line input), inserts a crime parameter into NODES and outputs an [XML OR OSM] file
 
 import xml.etree.ElementTree as ET
 import pickle
+import sys
 
 # read data
 output = open('crimedict.txt', 'rb')
 crimedict = pickle.load(output)    # 'obj_dict' is a dict object
 
-tree = ET.parse('data.osm')
+tree = ET.parse(sys.argv[1])
 root = tree.getroot()
 
-for child in root:
-    if child.tag == 'way':
+#for child in root:
+ #   if child.tag == 'way':
         #insert crime paramater
     #potentially get rid of superfluous info, e.g. usernames
