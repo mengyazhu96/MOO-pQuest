f0efafbf3d48decae9fd41a0b827a07ac06fc759
Jessica <mengyazhu96@gmail.com>
2015-04-28 03:51:01 +0000

    api adjustments

--- pyroute/add_to_node_id.py
+++ pyroute/add_to_node_id.py
@@ -2,8 +2,6 @@
 import sys
 from urllib2 import Request, urlopen, URLError
 
-
-
 #move these
 start_address = raw_input('Input starting address: ')
 
@@ -16,3 +14,16 @@ api_url_start = Request('http://www.mapquestapi.com/geocoding/v1/address?key=Fmj
 api_url_end = Request('http://www.mapquestapi.com/geocoding/v1/address?key=Fmjtd%7Cluu82q0bnl%2Caa%3Do5-94yxlf&location={}&callback=renderGeocode.'.format(end_address))
 
 try:
+    mapquest_reply_start = urlopen(api_url_start)
+    mapquest_reply_end = urlopen(api_url_end)
+    latlong_start = mapquest_reply_start.read()
+    latlong_end = mapquest_reply_end.read()
+  
+    data_start = json.loads(latlong_start)
+    data_end = json.loads(latlong_end)
+
+    print["results"]["locations"]["latLng"]["lng"]
+    print["results"]["locations"]["latLng"]["lat"]
+  
+except URLError, error:
+     print 'Error: ', error
