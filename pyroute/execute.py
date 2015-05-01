651f8dc3b2957b20d60c5408ef78f3955438f1d3
Jessica Zhu <mengyazhu96@Jessicas-MacBook-Pro-2.local>
2015-05-01 01:56:07 -0400

    distance

--- pyroute/execute.py
+++ pyroute/execute.py
@@ -7,6 +7,7 @@
 import sys
 import subprocess
 from gen_dict import *
+from math import sin, cos, atan2, sqrt, pi
 
 # parse user specification of crime types to ignore
 for k in CrimeWeights:
@@ -36,10 +37,40 @@ data = LoadOsm(sys.argv[1])
 # do the routing
 router = Router(data)
 result, route = router.doRouteAsLL(int(sys.argv[2]), int(sys.argv[3]), 'foot')
+abs_dist = router.distance(int(sys.argv[2]),int(sys.argv[3]))
+
+#print "Your destination is {} miles away".format(abs_dist)
+dist_input = raw_input('Please enter, in miles, the maximum distance you want to traverse (leave blank if unnecessary): ')
+max_dist = None
+if dist_input == '':
+	print 'Not accounting for distance.'
+elif abs_dist > float(dist_input):
+	print 'Input distance less than absolute distance between nodes. Ignoring distance input.'
+else:
+	max_dist = float(dist_input)
 
 # print out the results
 if result == 'success':
-	print "Route: %s" % ",".join("%1.4f,%1.4f" % (i[0],i[1]) for i in route)
+	# compute distance
+	distance = 0.0
+	for i in range(len(route)):
+		if i != (len(route) - 1):
+			dlat = abs(route[i+1][0] - route[i][0]) * (pi / 180)
+			dlon = abs(route[i+1][1] - route[i][1]) * (pi / 180)
+
+			# convert latlong into miles
+			a = (sin(dlat / 2.0))**2 + (cos(route[i+1][0] * (pi / 180)) * cos(route[i][0] * (pi / 180)) * (sin(dlon / 2.0))**2)
+			c = 2.0 * atan2(sqrt(a), sqrt(1.0 - a))
+			distance += ((3963.1676) * c)
+
+	if (max_dist is None) or max_dist > distance:
+		print
+		print "Route: %s" % ",".join("%1.4f,%1.4f" % (i[0],i[1]) for i in route)
+		print
+		print "Distance traveled: {} miles".format(distance)
+	else:
+		print "Could not find short enough route."
+
 else:
 	print "Failed (%s)" % result
 
